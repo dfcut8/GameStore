6380a79c87243afc9059591393504b64d61ebe14
dfcut8 <host1812@gmail.com>
2026-05-11 10:58:13 -0400

    first endpoint /games work as expected

---
+++ GameStore.Api/Dtos/GameData.cs
@@ -0,0 +1,3 @@
+﻿namespace GameStore.Api.Dtos;
+
+public record GameData(int Id, string Name, string Genre, decimal Price, DateOnly ReleaseDate);
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,23 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Game Store API.");
+
+List<GameData> games = new()
+{
+    new(1, "Street Fighter II", "Fighting", 19.99M, new DateOnly(2022, 11, 30)),
+    new(2, "Final Fantasy II", "JRPG", 39.99M, new DateOnly(1989, 06, 25)),
+    new(3, "Batman", "Beat em up", 29.99M, new DateOnly(1983, 03, 01)),
+};
+
+app.MapGet(
+    "/games",
+    () =>
+    {
+        return games;
+    }
+);
 
 app.Run();
