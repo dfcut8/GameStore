8d57fda64bd246704da22085609f18160c9b6bb5
dfcut8 <host1812@gmail.com>
2026-05-13 23:56:19 -0400

    using config for db context

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GameStore.Api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
 
@@ -9,5 +10,36 @@ public static class DataExtensions
         using IServiceScope scope = app.Services.CreateScope();
         GameStoreContext dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
         dbContext.Database.Migrate();
+    }
+
+    public static void AddGameStoreDb(this WebApplicationBuilder builder)
+    {
+        var connString = builder.Configuration.GetConnectionString("GameStore");
+
+        builder.Services.AddSqlite<GameStoreContext>(
+            connString,
+            optionsAction: ops =>
+                ops.UseSeeding(
+                    (ctx, _) =>
+                    {
+                        if (!ctx.Set<Genre>().Any())
+                        {
+                            ctx.Set<Genre>()
+                                .AddRange(
+                                    new Genre { Name = "JRPG" },
+                                    new Genre { Name = "TRPG" },
+                                    new Genre { Name = "Racing" },
+                                    new Genre { Name = "Action" },
+                                    new Genre { Name = "Horror" },
+                                    new Genre { Name = "Shoot Em Up!" },
+                                    new Genre { Name = "Sports" },
+                                    new Genre { Name = "2D Platformer" },
+                                    new Genre { Name = "Quest" }
+                                );
+                            ctx.SaveChanges();
+                        }
+                    }
+                )
+        );
     }
 }
